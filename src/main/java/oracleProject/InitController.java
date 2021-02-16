fce480068c67bdd970f3cdc456733c6a0d47b0e4
KWA <slimak-75@wp.pl>
2021-02-16 18:39:57 +0100

    INSERT i DELETE

--- src/main/java/oracleProject/InitController.java
+++ src/main/java/oracleProject/InitController.java
@@ -17,6 +17,16 @@ public class InitController {
 		return entitiesService.getAllEmployees();
 	}
 
+	@RequestMapping(value = "/employees", method = RequestMethod.PUT)
+	public void putEmployees(@RequestBody Employees employees) {
+		entitiesService.setEmployees(employees);
+	}
+
+	@RequestMapping(value = "/employeesD", method = RequestMethod.PUT)
+	public void putEmployeesD(@RequestBody Long id) {
+		entitiesService.deleteEmployees(id);
+	}
+
 	@RequestMapping("/countries")
 	public List<Countries> countries() {
 		return entitiesService.getAllCountries();
@@ -27,9 +37,24 @@ public class InitController {
 		entitiesService.setCountries(countries);
 	}
 
+	@RequestMapping(value = "/countriesD", method = RequestMethod.PUT)
+	public void putCountriesD(@RequestBody String id) {
+		entitiesService.deleteCountries(id);
+	}
+
 	@RequestMapping("/departments")
 	public List<Departments> departments() {
 		return entitiesService.getAllDepartments();
+	}
+
+	@RequestMapping(value = "/departments", method = RequestMethod.PUT)
+	public void putDepartments(@RequestBody Departments departments) {
+		entitiesService.setDepartments(departments);
+	}
+
+	@RequestMapping(value = "/departmentsD", method = RequestMethod.PUT)
+	public void putDepartmentsD(@RequestBody Long id) {
+		entitiesService.deleteDepartment(id);
 	}
 
 	@RequestMapping("/jobhistory")
@@ -37,9 +62,29 @@ public class InitController {
 		return entitiesService.getAllJobHistory();
 	}
 
+	@RequestMapping(value = "/jobhistory", method = RequestMethod.PUT)
+	public void putJobHistory(@RequestBody JobHistory jobHistory) {
+		entitiesService.setJobHistory(jobHistory);
+	}
+
+	@RequestMapping(value = "/jobhistoryD", method = RequestMethod.PUT)
+	public void putJobsHistoryD(@RequestBody JobHistoryId id) {
+		entitiesService.deleteJobHistory(id);
+	}
+
 	@RequestMapping("/jobs")
 	public List<Jobs> jobs() {
 		return entitiesService.getAllJobs();
+	}
+
+	@RequestMapping(value = "/jobs", method = RequestMethod.PUT)
+	public void putJobs(@RequestBody Jobs job) {
+		entitiesService.setJobs(job);
+	}
+
+	@RequestMapping(value = "/jobsD", method = RequestMethod.PUT)
+	public void putJobsD(@RequestBody String id) {
+		entitiesService.deleteJobs(id);
 	}
 
 	@RequestMapping("/locations")
@@ -47,8 +92,28 @@ public class InitController {
 		return entitiesService.getAllLocations();
 	}
 
+	@RequestMapping(value = "/locations", method = RequestMethod.PUT)
+	public void putLocations(@RequestBody Locations locations) {
+		entitiesService.setLocations(locations);
+	}
+
+	@RequestMapping(value = "/locationsD", method = RequestMethod.PUT)
+	public void putLocationsD(@RequestBody Long id) {
+		entitiesService.deleteLocations(id);
+	}
+
 	@RequestMapping("/regions")
 	public List<Regions> regions() {
 		return entitiesService.getAllRegions();
+	}
+
+	@RequestMapping(value = "/regions", method = RequestMethod.PUT)
+	public void putRegions(@RequestBody Regions regions) {
+		entitiesService.setRegions(regions);
+	}
+
+	@RequestMapping(value = "/regionsD", method = RequestMethod.PUT)
+	public void putRegionsD(@RequestBody Long id) {
+		entitiesService.deleteRegions(id);
 	}
 }
--- src/main/java/oracleProject/database/EntitiesService.java
+++ src/main/java/oracleProject/database/EntitiesService.java
@@ -30,31 +30,81 @@ public class EntitiesService {
         employeesRepo.save(employees);
     }
 
+    public void deleteEmployees(Long id) {
+        employeesRepo.deleteById(id);
+    }
+
     public List<Countries> getAllCountries() {
         return countriesRepository.findAll();
     }
 
+
     public void setCountries(Countries countries) {
         countriesRepository.save(countries);
+    }
+
+    public void deleteCountries(String id) {
+        countriesRepository.deleteById(id);
     }
 
     public List<Departments> getAllDepartments() {
         return departmentsRepository.findAll();
     }
 
+    public void setDepartments(Departments departments) {
+        departmentsRepository.save(departments);
+    }
+
+    public void deleteDepartment(Long id) {
+        departmentsRepository.deleteById(id);
+    }
+
     public List<JobHistory> getAllJobHistory() {
         return jobHistoryRepository.findAll();
     }
 
+    public void setJobHistory(JobHistory jobHistory) {
+        jobHistoryRepository.save(jobHistory);
+    }
+
+    public void deleteJobHistory(JobHistoryId id) {
+        jobHistoryRepository.deleteById(id);
+    }
+
+
     public List<Jobs> getAllJobs() {
         return jobsRepository.findAll();
+    }
+
+    public void setJobs(Jobs job) {
+        jobsRepository.save(job);
+    }
+
+    public void deleteJobs(String id) {
+        jobsRepository.deleteById(id);
     }
 
     public List<Locations> getAllLocations() {
         return locationsRepository.findAll();
     }
 
+    public void setLocations(Locations locations) {
+        locationsRepository.save(locations);
+    }
+
+    public void deleteLocations(Long id) {
+        locationsRepository.deleteById(id);
+    }
+
     public List<Regions> getAllRegions() {
         return regionsRepository.findAll();
+    }
+
+    public void setRegions(Regions regions) {
+        regionsRepository.save(regions);
+    }
+
+    public void deleteRegions(Long id) {
+        regionsRepository.deleteById(id);
     }
 }
